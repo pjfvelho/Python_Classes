53dcd9ee24d3ba5ac4e71b36e8463aa3df3e9714
pjfvelho <109762211+pjfvelho@users.noreply.github.com>
2023-09-29 23:58:01 +0100

    Update Lesson_0.py

--- Lesson_0.py
+++ Lesson_0.py
@@ -20,3 +20,11 @@ segs_por_min = 60
 total_secs = segs_por_min * mins_por_hora * horas_por_dia * dias_por_ano * num_anos
 print(total_secs)
 
+
+
+# TODO: Set the value of the births_per_min variable
+births_per_min = 250
+
+# TODO: Set the value of the births_per_day variable
+births_per_day = births_per_min * mins_per_hour * hours_per_day
+
