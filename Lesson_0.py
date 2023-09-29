2977872033710f071afec67e1a3a754acf27b0a7
pjfvelho <109762211+pjfvelho@users.noreply.github.com>
2023-09-29 23:48:48 +0100

    Update Lesson_0.py

--- Lesson_0.py
+++ Lesson_0.py
@@ -14,3 +14,9 @@ dias_por_ano = 365
 horas_por_dia = 24
 mins_por_hora = 60
 segs_por_min = 60
+
+
+# Calcular o número de sugundos em 4 anos
+total_secs = segs_por_min * mins_por_hora * horas_por_dia * dias_por_ano * num_anos
+print(total_secs)
+
