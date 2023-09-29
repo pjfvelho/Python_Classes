41fd0d0a7eab9dcda4337f459fcd62bf8c38ab0f
pjfvelho <109762211+pjfvelho@users.noreply.github.com>
2023-09-30 00:10:53 +0100

    Update Lesson_0.py

--- Lesson_0.py
+++ Lesson_0.py
@@ -28,3 +28,42 @@ births_per_min = 250
 # TODO: Set the value of the births_per_day variable
 births_per_day = births_per_min * mins_per_hour * hours_per_day
 
+
+#Conteúdo 2: Exemplo de estrutura condicional (if-else)
+
+idade = 18
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+
+#Conteúdo 3: Exemplo de loop for para listar números pares
+
+for i in range(2, 11, 2):
+    print(i)
+
+
+#Conteúdo 4: Exemplo de função para calcular a média de uma lista de números
+
+def calcular_media(lista):
+    soma = sum(lista)
+    media = soma / len(lista)
+    return media
+
+numeros = [10, 20, 30, 40, 50]
+media = calcular_media(numeros)
+print("A média dos números é:", media)
+
+
+#Conteúdo 5: Exemplo de leitura e escrita em arquivos de texto
+
+# Escrevendo em um arquivo
+with open("exemplo.txt", "w") as arquivo:
+    arquivo.write("Olá, mundo!")
+
+# Lendo de um arquivo
+with open("exemplo.txt", "r") as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
+
