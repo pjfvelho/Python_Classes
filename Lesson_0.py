27d495ee788a2ccd4665fc3386f332ac1896aa9c
pjfvelho <109762211+pjfvelho@users.noreply.github.com>
2023-10-01 11:27:08 +0100

    Update Lesson_0.py

--- Lesson_0.py
+++ Lesson_0.py
@@ -67,3 +67,15 @@ with open("exemplo.txt", "r") as arquivo:
     conteudo = arquivo.read()
     print(conteudo)
 
+
+# Definir função
+def add_three(input_var):
+    output_var = input_var + 3
+    return output_var
+
+
+# Run the function with 10 as input
+new_number = add_three(10)
+
+# Check that the value is 13, as expected
+print(new_number)
