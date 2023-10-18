e99d03c8ebe46352e1f67596104703004aedaa5f
pjfvelho <109762211+pjfvelho@users.noreply.github.com>
2023-10-18 15:47:34 +0100

    Update and rename programa1_nome.py to Programa1_nome.py

---
+++ Programa1_nome.py
@@ -0,0 +1,11 @@
+
+print("Por favor, preencha a seguinte informação")
+primeiro_nome = input("Primeiro nome: ")
+ultimo_nome = input("Último nome: ")
+idade = input("Idade: ")
+email = input("Email: ")
+
+print("\nFirst name:", first_name)
+print("Last name:",last_name)
+print("Age:",age)
+print("Email:",email)
